a5965c5391d600e7c3418d1d486c1f73a2721f79
Bibek <bibek9818917036@gmail.com>
2023-08-07 01:34:20 +1000

    Had finished getting quotes and setting maximum limits of 10

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+  const [quotes, setQuotes] = useState(0);
+
+  async function handleClick() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    if (quotes >= 10) {
+      return alert("You have reached maximum daily quotes ");
+    }
+    setQuotes(quotes + 1);
+    setText(data.slip.advice);
+  }
+
   return (
     <div className="App">
-      <h1>Hello Daily quotes</h1>
+      <h1>Awesome Quotes</h1>
+      <p>{text}</p>
+      <div>
+        <button onClick={handleClick}>Get Quotes</button>
+      </div>
+      <h3>You have requested {quotes} quotes.</h3>
     </div>
   );
 }
